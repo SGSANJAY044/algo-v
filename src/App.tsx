dd5cc7a64ee4394b962434d4f310a1a77e914b80
SGSANJAY <93423319+SGSANJAY044@users.noreply.github.com>
2023-08-24 18:09:08 +0530

    review

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import { bubbleSort, heapSort, mergeSort, quickSort } from './algorithms';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    interface Bar {
+        value: number,
+        color: string
+    }
+    enum Algorithms {
+        quickSort = 'Quick Sort',
+        mergeSort = 'Merge Sort',
+        heapSort = 'Heap Sort',
+        bubbleSort = 'Bubble Sort'
+    }
+    const [algo, setalgo]: any = useState(Algorithms.quickSort);
+    const [array, setarray] = useState<Bar[]>([]);
+    const [change, setchange]: any = useState(0.8);
+    const size = 80;
+    useEffect(() => {
+        resetArray();
+    }, [])
+    function resetArray() {
+        let temp: Bar[] = [];
+        for (let i = 0; i < size; i++) {
+            let min = 20, max = 500;
+            temp.push({ value: Math.floor(Math.random() * (max - min + 1) + min), color: "#7CB9E8" });
+        }
+        setarray(temp);
+    }
+    function handleAlgoChange(e: any) {
+        setalgo(e.currentTarget.value);
+        resetArray();
+    }
+    async function sort() {
+        let arr = array;
+        console.log("before: ", arr);
+        switch (algo) {
+            case Algorithms.quickSort:
+                await quickSort(arr, 0, arr.length - 1, setchange);
+                break;
+            case Algorithms.mergeSort:
+                await mergeSort(arr, 0, arr.length - 1, setchange);
+                break;
+            case Algorithms.bubbleSort:
+                await bubbleSort(arr, setchange);
+                break;
+            case Algorithms.heapSort:
+                await heapSort(arr, setchange);
+                break;
+            default:
+                break;
+        }
+        console.log("after: ", arr);
+    }
+
+    return (
+        <>
+           
+            <div className="head">
+                <div className="frame">
+                    <div className="boundary-sorting">
+                        {array.map((e: Bar, idx) => <div className="bar" key={idx} style={{ height: e.value, background: e.color, width: (window.innerWidth * 0.6) / size }}></div>)}
+                    </div>
+                </div>
+            </div>
+            <div className="nav">
+                <h3>Sorting Algorithm: {algo}</h3>
+                <select onChange={handleAlgoChange}>
+                    <option>{Algorithms.quickSort}</option>
+                    <option>{Algorithms.mergeSort}</option>
+                    <option>{Algorithms.heapSort}</option>
+                    <option>{Algorithms.bubbleSort}</option>
+                </select>
+                <button onClick={resetArray}>Reset</button>
+                <button onClick={sort}>Sort</button>
+
+            </div>
+        </>
+    )
 }
 
 export default App;
---
+++ src/algorithms.ts
@@ -0,0 +1,112 @@
+const delay=10;
+async function partition(arr: any[],minIndex:number,maxIndex:number,setchange:Function):Promise<number>{
+    var pivot=maxIndex;
+    let i=minIndex-1,j;
+    for(j=i+1;j<pivot;j++){
+        if(arr[j].value<arr[pivot].value){
+            i+=1;
+            await swap(arr,i,j,setchange);
+        }
+    }
+    i+=1;
+    await swap(arr,i,pivot,setchange);
+    return i;
+}
+export async function sleep(delay : number){
+    await new Promise(r => setTimeout(r, delay));
+}
+async function swap(arr: any[],i:number,j:number,setchange:Function):Promise<void>{
+    let temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    arr[i].color="green";
+    arr[j].color="green";
+    await sleep(delay);
+    arr[i].color="#7CB9E8";
+    arr[j].color="#7CB9E8";
+    setchange(Math.random());
+}
+export async function quickSort(arr:any[],minIndex:number,maxIndex:number,setchange:Function):Promise<void>{
+    if(maxIndex<=minIndex){
+        return;
+    }
+    let pivot=await partition(arr,minIndex,maxIndex,setchange);
+    await quickSort(arr,minIndex,pivot-1,setchange);
+    await quickSort(arr,pivot+1,maxIndex,setchange);
+}
+
+export async function mergeSort(arr:any[],low:number,high:number,setchange:Function) {
+    let m= Math.floor((low+high)/2);
+    if(high<=low){
+        return;
+    }
+    await mergeSort(arr,low,m,setchange);
+    await mergeSort(arr,m+1,high,setchange);
+
+    let j=m+1,i=low,k=0;
+    let t=[];
+    for (; i <= m && j<=high;) {
+            if(arr[i].value<arr[j].value){
+                t[k]=arr[i];
+                i++;
+            }else{
+                t[k]=arr[j];
+                j++;
+            }
+            k++;
+    }
+    for(;i<=m;){
+        t[k]=arr[i];
+        i++;
+        k++;
+    }
+    for(;j<=high;){
+        t[k]=arr[j];
+        j++;
+        k++;
+    }
+    for(i=low,k=0;i<=high;i++){
+        arr[i]=t[k++];
+        await sleep(delay);
+        setchange(Math.random());
+    }
+}
+
+export async function bubbleSort(arr:any[],setchange:Function) {
+    let i,j;
+    for(i=0;i<arr.length-1;i++){
+        for(j=i;j<arr.length;j++){
+            if(arr[i].value>arr[j].value){
+                await swap(arr,i,j,setchange);
+            }
+        }
+    }
+}
+
+async function heapify(arr:any[],n:number,i:number,setchange:Function) {
+    let largest=i,l,r;
+    l=2*i +1;
+    r=2*i +2;
+
+    if(l<n && arr[largest].value<arr[l].value){
+        largest=l;
+    }
+    if(r<n && arr[largest].value<arr[r].value){
+        largest=r;
+    }
+    if(largest!=i){
+        await swap(arr,i,largest,setchange);
+        await heapify(arr,n,largest,setchange);
+    }
+    
+}
+export async function heapSort(arr:any[],setchange:Function) {
+    let i,n=arr.length;
+    for(i=Math.floor(n/2)-1;i>=0;i--){
+        await heapify(arr,n,i,setchange);
+    }
+    for(i=n-1;i>0;i--){
+        await swap(arr,i,0,setchange);
+        await heapify(arr,i,0,setchange);
+    }
+}
